7a1c03f7254ce3bf20929e38333db559bc153b15
Joseph Park <jhspark17@gmail.com>
2019-10-04 17:40:06 -0700

    Halfway through tutorial

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
+const passport = require("passport")
 const port = process.env.PORT || 5000;
-const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
-app.get("/", (req, res) => res.send("Fuck You"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
-mongoose.connect(db, { useNewUrlParser: true})
-.then(() => console.log("Connected to MongoDB successfully"))
-.catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize())
+require('./config/passport')
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+// app.get("/", (req, res) => {
+//   res.send("hello");
+// });
---
+++ routes/api/users.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require('passport');
+
+router.get("/test", (req, res) => {
+  res.json({ msg: "This is the users route" });
+});
+
+router.get("/current", passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({msg: "success"})
+})
+
+router.post("/register", (req, res) => {
+  // const {errors, isValid} = validRegisterInput(req.body);
+  // if (!isValid) return res.status(400).json(errors);
+
+  User.findOne({ email: req.body.email }).then(user => {//CHECK FIND ONE MAYBE: USER.findOne({name: req.body.name})
+    if (user) {
+      //Throw a 400 error if the email address already exists
+     errors.email = "Email is already taken"
+     return res.status(400).json({email: "Already Taken"});
+    } else {
+      //Otherwise create a new user
+      const newUser = new User({
+        handle: req.body.handle,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.send(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkOTdhYzdiODM4ZmQ4OWVhNzRkYzNhMSIsImlhdCI6MTU3MDIyNzE0MiwiZXhwIjoxNTcwMjMwNzQyfQ.aMzG9c6dkLVQ0w-8SnOSm3l7KHFJZ_rlpQPlBlXqY08
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user)
+      return res.status(404).json({ email: "This user does not exist" });
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({msg: "Success!"})
+      } else return res.status(400).json({ password: "Incorrect Password" });
+    });
+  });
+});
+
+module.exports = router;
+
+
+//register
+// {
+//   const payload = {id: user.id, name: user.handle}
+//   jwt.sign(payload, keys.secretOrKey, {expiresIn: 3600}, 
+//     (err, token) => {
+//       res.json({
+//         success: true,
+//         token: "Bearer " + token
+//       })
+//     })
+// }
+
+
+
+//login
+// const payload = {
+//   id: user.id,
+//   name: user.name
+// };
+
+// jwt.sign(
+//   payload,
+//   keys.secretOrKey,
+//   { expiresIn: 3600 },
+//   (err, token) => {
+//     res.json({
+//       success: true,
+//       token: `Bearer ${token}`
+//     });
+//   }
+// );
